3f9262d2a746b11728d27c02dc8cfb89ed7d02c6
Michael Nogueira <mvn_14@outlook.com>
2024-07-20 13:16:53 -0400

    Adding kemap for Ctrl-c to close harpoon
    Also added some descriptions to keymaps

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,10 +1,12 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set('n', '<leader>a', mark.add_file)
-vim.keymap.set('n', '<C-h>', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>a', mark.add_file, { desc = "Harpoon: [A]dd File" })
+vim.keymap.set('n', '<C-h>', ui.toggle_quick_menu, { desc = "Harpoon: Toggle Quick Menu" })
 
-vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end, { desc = "Harpoon: Nav File [1]" })
+vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end, { desc = "Harpoon: Nav File [1]" })
+vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end, { desc = "Harpoon: Nav File [1]" })
+vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end, { desc = "Harpoon: Nav File [1]" })
+
+vim.keymap.set('n', '<C-c>', ui.toggle_quick_menu, { desc = "Harpoon: Toggle Quick Menu" })
